51dcd9fa0cf98c240398242b923dd6c6ba21f207
himased1214 <chanakan_c@elearning.cmu.ac.th>
2020-03-24 16:48:00 +0700

    Updates

---
+++ precache-manifest.42676fc6042eb29230b9f7f156221443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b5fac91e6d9b3f240c008e0eb4cf0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83e7e922bf360c82c6ff",
+    "url": "/static/css/main.28b3815d.chunk.css"
+  },
+  {
+    "revision": "791b884cc2e3e2aaf02c",
+    "url": "/static/js/2.06e8f7b9.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/static/js/2.06e8f7b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83e7e922bf360c82c6ff",
+    "url": "/static/js/main.0cf86dfc.chunk.js"
+  },
+  {
+    "revision": "0d3bbb528df5eb49ddfc",
+    "url": "/static/js/runtime-main.caa9dce7.js"
+  }
+]);
