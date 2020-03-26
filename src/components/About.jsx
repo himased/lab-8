b6ecbcdeac5b48b5b69483e1641f8e5d115452c4
himased1214 <chanakan_c@elearning.cmu.ac.th>
2020-03-26 19:46:37 +0700

    lul2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,9 +9,9 @@ export default function About() {
           <img src={process.env.PUBLIC_URL+'/GiornoAV (2).png'} className='card-img-top' alt="..." />
           <div className="card-body">
             <h5 className="card-title">Developer information</h5>
-            <p>Theerasuwat Thungthanaphon (610610590)</p>
+            <p>Chanakan Chaiwong (610610576)</p>
             <p>This app use Google Firebase as backend.</p>
-            <Link to="/Labilly-8/">
+            <Link to="/Lab-8/">
               <h3>
                 <button type="button" className="btn btn-primary">Home</button>
               </h3>
